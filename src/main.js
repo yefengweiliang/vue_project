e3b30c5dcd819663986a0172bb09e53854db01a2
Admin <925209468@qq.com>
2020-05-17 15:32:17 +0800

    完成了登錄功能

---
+++ src/main.js
@@ -0,0 +1,27 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router/router.js'
+import './assets/css/global.css'
+import Element, { Message } from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+//导入字体图标
+import './assets/fonts/iconfont.css'
+import axios from 'axios'
+axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded'
+axios.defaults.baseURL = 'http://localhost:8070/'
+Vue.prototype.$http = axios
+Vue.prototype.$message = Message
+
+Vue.use(Element)
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
---
+++ src/router/router.js
@@ -0,0 +1,35 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Login from '../components/Login.vue'
+import Home from '../components/home.vue'
+
+Vue.use(Router)
+
+const router = new Router(
+  {
+    routes: [
+      { path: '/', redirect: '/login' },
+      { path: '/login', component: Login },
+      { path: '/home', component: Home }
+    ]
+  }
+)
+
+//挂载路由导航守卫
+router.beforeEach((to, from, next) => {
+  // to将要访问的路径
+  // from代表从哪个路径跳转而来
+  // next 是一个函数。表示放行
+  // next() 放行 next('/login') 强制跳转
+  if (to.path === '/login') {
+    return next()
+  }
+  //获取token
+  const tokenStr = window.sessionStorage.getItem('token')
+  if (!tokenStr) {
+    return next('/login')
+  } else {
+    next()
+  }
+})
+export default router
